e9389b4a0b3c482bf80a2383aada68d0203a15a4
r.wasik <kontakt.radek96@gmail.com>
2026-09-06 11:01:48 +0200

    fix: import auth module in tests catalog

--- apps/api/src/tests-catalog/tests-catalog.module.ts
+++ apps/api/src/tests-catalog/tests-catalog.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
+import { AuthModule } from "../auth/auth.module";
 import { TestsCatalogController } from "./tests-catalog.controller";
 import { TestsCatalogService } from "./tests-catalog.service";
 
 @Module({
+  imports: [AuthModule],
   controllers: [TestsCatalogController],
   providers: [TestsCatalogService]
 })
